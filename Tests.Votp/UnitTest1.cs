81f7fba6bc6c0a642c349a23fd2df61bd1a63f58
Uladzimirant <vladimir.bwmail@gmail.com>
2023-03-16 01:13:44 +0300

    fixed tests

--- Tests.Votp/UnitTest1.cs
+++ Tests.Votp/UnitTest1.cs
@@ -3,6 +3,7 @@ using AutoMapper.Internal;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Moq;
+using Moq.AutoMock;
 using System.Data;
 using Votp;
 using Votp.Contracts.Services;
@@ -16,18 +17,39 @@ namespace Tests.Votp
 {
     public class TokenControllerTest
     {
-        private TokensController CreateTokenController(IVotpDbContext votpDbContext)
+        private static List<Token> GenList()
+        {
+            User[] user =
+{
+                    new User{ Id = 1, Login = "U1" },
+                    new User{ Id = 2, Login = "U2" },
+                    new User{ Id = 3, Login = "U3" }
+                };
+            return new List<Token>()
+            {
+                    new Token() { Id = 1, User = user[0], Value = "1", RegistrationTime = DateTime.Now },
+                    new Token() { Id = 2, User = user[1], Value = "2", RegistrationTime = DateTime.Now },
+                    new Token() { Id = 3, User = user[2], Value = "3", RegistrationTime = DateTime.Now }
+            };
+        }
+
+        private TokensController CreateTokenController(out List<Token> innerList)
         {
             var lfact = LoggerFactory.Create(e => { });
             var map = new MapperConfiguration(c => c.AddProfile(new AutoMapperProfile())).CreateMapper();
 
+            //var dbservice = new DBTokenService(lfact.CreateLogger<DBTokenService>(), map, votpDbContext);
+            var mockToken = new Mock<ITokenService>();
+            var list = GenList();
+            mockToken.Setup(a => a.CreateToken(It.IsAny<Token>())).Callback<Token>((t)=>list.Add(t));
+            mockToken.Setup(a => a.GetTokens()).ReturnsAsync(list);
+            ITokenService tokenService = mockToken.Object;
+            IUserService userService = Mock.Of<IUserService>();
 
-            var dbservice = new DBTokenService(lfact.CreateLogger<DBTokenService>(), map, votpDbContext);
-            ITokenService tokenService = dbservice;
-            IUserService userService = dbservice;
 
             TokensController tokensController
                 = new TokensController(lfact.CreateLogger<TokensController>(), map, tokenService, userService);
+            innerList = list;
             return tokensController;
         }
 
@@ -57,37 +79,13 @@ namespace Tests.Votp
         public void CreateToken_Correct(string token, string username)
         {
             //arrange
-            var options = new DbContextOptionsBuilder<VotpDbContext>().UseInMemoryDatabase(databaseName: "Votp").Options;
-            using (var context = new VotpDbContext(options, Mock.Of<ITokenLibService>()))
-            {
-                DefaultFillDB(context);
-                TokensController controller = CreateTokenController(context);
+            TokensController controller = CreateTokenController(out var list);
 
-                //act
-                controller.Create(new TokenIDto() { Value = token, UserName = username }).Wait();
+            //act
+            controller.Create(new TokenIDto() { Value = token, UserName = username }).Wait();
 
-                //assert
-                Assert.Contains(context.Tokens, e => e.Value == token);
-            }
-        }
-
-        [Theory]
-        [InlineData("4", "U4")]
-        public void CreateToken_Incorrect(string token, string username)
-        {
-            //arrange
-            var options = new DbContextOptionsBuilder<VotpDbContext>().UseInMemoryDatabase(databaseName: "Votp").Options;
-            using (var context = new VotpDbContext(options, Mock.Of<ITokenLibService>()))
-            {
-                DefaultFillDB(context);
-                TokensController controller = CreateTokenController(context);
-                //act
-                //assert
-                Assert.Throws<AggregateException>(() =>
-                {
-                    controller.Create(new TokenIDto() { Value = token, UserName = username }).Wait();
-                });
-            }
+            //assert
+            Assert.Contains(list, e => e.Value == token);
         }
 
     }
