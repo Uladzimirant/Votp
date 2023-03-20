85ee5382ba6a199a727106f3d6d43b61abad60f6
Uladzimirant <vladimir.bwmail@gmail.com>
2023-03-20 20:50:02 +0300

    Database generating only in dev and added GitVersion

--- Votp/Program.cs
+++ Votp/Program.cs
@@ -129,17 +129,19 @@ namespace Votp
 
             var app = builder.Build();
 
-            using (var db = app.Services.CreateScope().ServiceProvider.GetRequiredService<IVotpDbContext>() as VotpDbContext)
-            {
-                if (db != null)
-                {
-                    InitializeDB(db);
-                }
-            }
+
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
+                using (var db = app.Services.CreateScope().ServiceProvider.GetRequiredService<IVotpDbContext>() as VotpDbContext)
+                {
+                    if (db != null)
+                    {
+                        InitializeDB(db);
+                    }
+                }
+
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
