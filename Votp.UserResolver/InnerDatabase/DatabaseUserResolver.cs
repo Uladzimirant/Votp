f0b16907f3f262a7d4b43db467a50f1eae112e5f
Uladzimirant <vladimir.bwmail@gmail.com>
2023-04-13 01:23:53 +0300

    Added Ldap Resolver

---
+++ Votp.UserResolver/InnerDatabase/DatabaseUserResolver.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Votp.Contracts.Services.UserResolver;
+using Votp.DS.Database;
+using Votp.DS.Database.Entities;
+
+namespace Votp.UserResolver.InnerDatabase
+{
+    public class DatabaseUserResolver : IResolver<User>
+    {
+        IServiceProvider _provider;
+        public DatabaseUserResolver(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+        public IEnumerable<User> GetResolvedList()
+        {
+            using (var scope = _provider.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<IVotpDbContext>();
+                return db.Users.ToList();
+            }
+        }
+    }
+}
---
+++ Votp.UserResolver/InnerDatabase/EntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Votp.DS.Database.Entities;
+using Votp.UserResolver.InnerDatabase;
+
+namespace Votp.UserResolver.Ldap
+{
+    //public class EntityConfiguration : IEntityTypeConfiguration<DatabaseUserResolverInfo>
+    //{
+    //    public void Configure(EntityTypeBuilder<DatabaseUserResolverInfo> builder)
+    //    {
+    //        builder.HasBaseType<ResolverInfo>();
+    //    }
+    //}
+}
